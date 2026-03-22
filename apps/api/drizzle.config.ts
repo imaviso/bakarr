f6d86915ad287d681a20517e5f6703a62cbd7dee
imaviso <mail@imaviso.com>
2026-03-22 11:43:06 +0800

    chore(deps): update dependencies

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,9 +1,7 @@
 import process from "node:process";
 import { defineConfig } from "drizzle-kit";
 
-const databaseFile = typeof Deno !== "undefined"
-  ? Deno.env.get("DATABASE_FILE")
-  : process.env.DATABASE_FILE;
+const databaseFile = process.env.DATABASE_FILE;
 
 export default defineConfig({
   out: "./drizzle",
