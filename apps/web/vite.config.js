c189c53aba0884e290be3fad3f831699258c025f
Momoyaan <ianmathewaviso@gmail.com>
2026-04-11 14:08:10 +0800

    fix(web): add ws upgrade

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -30,6 +30,7 @@ export default defineConfig(({ mode }) => {
         "/api": {
           target: apiTarget,
           changeOrigin: true,
+          ws: true,
         },
         "/images": {
           target: apiTarget,
